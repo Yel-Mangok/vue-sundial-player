bcf875560d3151e41a4c3fb2492d8e26cfc77ac4
YEL MANGOK JOHN <yelmangok1992@gmail.com>
2026-07-02 17:22:46 +0200

    App component updated

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,17 @@
 <script setup>
+import Navbar from "./components/Navbar.vue";
+import usePlayer  from "./composables/usePlayer.js";
+import "./styles/App.css";
+import "./styles/global.css";
 
 </script>
 
 <template>
-  <HelloWorld />
+
+   <div class="app-shell">
+      <Navbar />
+      <main class="app-main">
+         <RouterView />
+      </main>
+    </div>
 </template>
